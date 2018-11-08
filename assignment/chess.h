e472e0db66f5187a7e03b2046185b6a9e8a096f9
LucaTomala <43435523+LucaTomala@users.noreply.github.com>
2018-11-08 09:17:49 +0100

    forgot to commit it at home

--- assignment/chess.h
+++ assignment/chess.h
@@ -1,12 +1,28 @@
-/*----------------------------------------------------------
- *				HTBLA-Leonding / Klasse: 2AHDV
- * ---------------------------------------------------------
- * Exercise Number: 0
- * Title:			chess.h
- * Author:			P. Bauer
- * Due Date:		October 15, 2014
- * ----------------------------------------------------------
- * Description:
- * Basic chess functions.
- * ----------------------------------------------------------
- */
+ #include <stdbool.h>
+ #ifndef ___CHESS_H
+ #define ___CHESS_H
+ enum Color
+ {
+   White, Black
+ };
+
+ enum PawnMoves
+ {
+   CaptureMove, NormalMove
+ };
+ enum PieceType
+ {
+   Pawn, Rook, Knight, Bishop, Queen, King, NoPiece
+ };
+
+ struct ChessPiece
+ {
+     enum Color color;
+     enum PieceType type;
+ };
+
+ struct ChessSquare
+ {
+     bool is_occupied;
+     struct ChessPiece piece;
+ };
